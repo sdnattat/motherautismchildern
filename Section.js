7a6768c436b8dd175e4e747119f0e9565b3a5a2f
sdnattat <56126962+sdnattat@users.noreply.github.com>
2020-03-23 02:50:04 +0200

    Create Section.js

---
+++ Section.js
@@ -0,0 +1,38 @@
+//<![CDATA[
+
+// ------------------------------------
+// Template Configuration
+// ------------------------------------
+
+var DefaultConfig = {
+
+	// ------------------------------------
+	// Translation Section
+	// ------------------------------------
+    abbrMonthName: ["يناير","فبراير","مارس","أبريل","مايو","يونيو"," يوليو"," أغسطس","سبتمبر","أكتوبر","نوفمبر"," ديسمبر"],
+    readMore: "اقرأ المزيد",
+    search:  "ابحث هنا...",
+  	relatedArticles: "مقالات ذات صلة",
+    latestArticles: "أحدث المقالات",	
+    nextPost: "الموضوع التالي",
+    prevPost: "الموضوع السابق",
+  	tag: "التصنيفات",
+
+
+	// ------------------------------------
+	// Custom Configuration
+	// ------------------------------------
+    perPage: 4,  // Number Of Posts Per Page 
+    noThumb: "https://lh3.googleusercontent.com/-Yyf6Amv22oI/VcDFF690PYI/AAAAAAAACT8/NSB63gjG9NM/s840-Ic42/NoThumb.png", // No Thumb Image
+  	breaking: 5, // Number Of Breaking News Posts
+
+
+	// ------------------------------------
+	// DO NOT CHANGE THIS
+	// ------------------------------------
+	numPages: 6,
+	monthNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    blogUrl: window.location.protocol + "//" + window.location.host
+
+};
+//]]>
