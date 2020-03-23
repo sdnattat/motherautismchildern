b7b2b937df4ba373bd0318a38290cbbb5cb5cfce
sdnattat <56126962+sdnattat@users.noreply.github.com>
2020-03-23 02:43:09 +0200

    Create icon.js

---
+++ icon.js
@@ -0,0 +1,46 @@
+.column,.columns,.tickerfloat_wrapper,.tickerfloat_wrapper,.sf-menu &gt;li,.meta-category-small,.menu_post_feature .post-meta .post-autho i,.main-post-image-slider,.post-meta.meta-main-img,.widget-title h2,.post-meta span,ul.tab-wrapper li,.feature-post-list .image_post,.recent-comments.post-list a,.menu_post_feature .wrap_box_style_main .post-meta .post-autho a,.feature-post-list .post_loop_content,.meta-category-small a,.mega_labels_list,.owl_slider .item_slide_caption h1,.wrap_box_style_main,.wrap_box_style_ul,.footer_meta .footer_meta_readmore,#search_block_top #search_query_top,#search_block_top button,.slider-large .post-meta i,.homepage .post-outer .post-meta i,.single-post .post-meta .author-avata,.tag-cat .tag_title,.tag-cat a,.share-post li,.owl_carousel_builder_2col .image-post-title,#mycrawler div,.breadcrumbs_options span,.breadcrumbs_options i,#ticker a.ticker_title,.ticker_dot,#comment-holder .avatar-image-container,#comment-holder .comment-header .blog-autho,.grid_3.grid,.autho-inf .autho-avatar{float:right!important}
+#search_block_top,.social-icons-list.top-bar-social,.social-icons-list.top-bar-social li,.sf-menu &gt;li.has-menu &gt;a:after,.footer_meta .meta-commentd,.list-label-widget-content li span:nth-child(2),#comment-holder .comment-header .datetime,#pageslide .has-children &gt;a:after,.more_button_post{float:left!important}
+#mainmenu li &gt;a i,.post-meta .post-autho,.post-meta .post-date,.item_slide_caption .post-meta .meta-cat,#comment-holder .comment-actions &gt;a:before,.meta-category-small a,#search_block_top button,.footer_meta .meta-commentd i,.tag-cat .tag_title,.tag-cat a,#mycrawler div i,#comment-holder .comment-actions span a:before,#pageslide a i,.tickerfloat{margin:0;margin-left:5px}
+.random_post_link,.megamenu .sub-menu{left:0;right:auto}
+.sf-menu ul{right:0;left:auto}
+.owl_slider .item_slide_caption h1,.image-post-title,.post-body,.post-body &gt;div,.post-body span,.post-body p{text-align:riight!important}
+.owl-carousel{direction:ltr}
+#search_block_top{margin:0;margin-right:10px}
+#search_block_top button{margin-top:8px!important;color:#dddddd57}
+.sf-menu &gt;li.has-menu &gt;a:after{margin:0;margin-right:4px;margin-top:18px}
+.sidebar{margin:0;margin-right:2.12766%}
+.post-meta.meta-main-img i{margin-left:3px}
+.random_post_link{background-position:top right;display:none}
+.widget-title h2{background:$(theme.color);margin-bottom:0;padding:3px 10px;color:#fff;display:inline-block;border-radius:50px;height:29px;vertical-align:middle;text-transform:capitalize;margin:5px;font-size:12px;line-height:24px;font-weight:400;letter-spacing:.5px}
+.three_columns_container .content_display_col1{padding:0;padding-left:2.12766%}
+ul.tab-wrapper li{border:none;border-left:1px solid #DDD}
+.feature-post-list .item-details{margin:0;margin-right:95px}
+.recent-comments.post-list a,.single-post .post-meta .author-vatar{margin:0;margin-left:13px;width:25px;height:25px;margin-top:-2px}
+.feature-post-list .post_loop_content{margin:0;margin-right:23px}
+.left-post-display-content{padding-right:0!important;padding-left:3%;float:right!important;border-right:none!important;border-left:1px solid #e6e6e6}
+.feature-two-column.right-post-display-content{padding-left:0!important;padding-right:3%!important}
+.three_columns_container .content_display_col1{border-right:none;border-left:1px solid #e6e6e6}
+.home_post_right_list_text .feature-post-list .item-details{margin:0}
+.wrap_box_style_main{margin-bottom:0}
+.header-top-right-bar,#header-ads{margin:0;margin-right:2.12766%}
+.owl_carousel_builder_2col .post-meta i{float:right;margin-top:3px}
+.slider-large .post-meta i{margin-top:10px}
+.homepage .post-outer .post-meta i{margin-top:3px}
+.list-label-widget-content li:before{content:&#39;&#39;;margin:0;margin-left:8px}
+.share-post li{margin:2px 0 2px 5px;font:12px &#39;sky&#39;!important}
+.breadcrumbs_options{overflow:hidden}
+.breadcrumbs_options i{margin:5px 5px 0}
+.post-body blockquote:first-child{padding:40px 60px 40px 50px;text-align:right}
+blockquote:first-child:before{left:auto;right:30px}
+.author-inf .autho-description{margin-left:0;margin-right:102px}
+#comment-holder .comment-actions &gt;a{margin:0;margin-left:10px}
+#comment-holder .thread-toggle .thread-arrow{padding:0;padding-left:5px}
+#comment-holder .avatar-image-container{margin:0 0 10px 10px}
+#pageslide{-webkit-transform:translateX(300px);transform:translateX(300px)}
+#pageslide #nav &gt;ul &gt;li a{padding-left:0;padding-right:30px}
+#pageslide #nav ul ul &gt;li a{padding-left:0;padding-right:40px}
+#pageslide #nav ul ul ul &gt;li a{padding-left:0;padding-right:50px}
+#pageslide #nav ul ul ul ul &gt;li a{padding-left:0;padding-right:60px}
+#pageslide .has-children &gt;a:after{margin-right:0;margin-left:15px}
+.footer-columns .four.columns:last-child{margin-left:0!important}
+.footer-columns .four.columns:first-child{margin-left:5.12766%!important}
